c7ddc3e7a75979e7314ae5d5797ca67c649579d1
Smitkumar Contractor <smitkumarcontractor@vpn-campus-214.csuchico.edu>
2018-10-02 23:38:06 -0700

    added comments to stepN and max_subarray

--- max_subarray/main.cpp
+++ max_subarray/main.cpp
@@ -1,19 +1,31 @@
+/* file_name : main.cpp
+ * author_name : Smitkumar Contractor
+ * contact : smitcontractor@gmail.com
+ * brief : this program tries to find the max sum continuous subarray.
+ */
+
+// includes
 #include <iostream>
 
+// using statements
 using std::cout;
 using std::endl;
 
+// main
 int main() {
   int n = 5;
   int a[n] = {1, -3, 2, 1, -1};
-
+  // global maximum
   int max_so_far = a[0];
+  // curr_sum_of_sub_array
   int end = a[0];
   for (int i = 1; i < n; i++) {
     end += a[i];
+    // if global max is less than curr_sum_of_sub_array, UPDATE global max
     if (max_so_far < end) {
       max_so_far = end;
     }
+    // if curr_sum_of_sub_array is less than 0 update it to 0.
     if (end < 0) {
       end = 0;
     }
--- stepN/main.cpp
+++ stepN/main.cpp
@@ -1,14 +1,29 @@
+/* file_name : main.cpp
+ * author_name : Smitkumar Contractor
+ * contact : smitcontractor@gmail.com
+ * brief : Climbing Stairs (you can climb 1, 2 or 3 stairs with one step).
+ *        How many diffrent ways can you climb n stairs?
+ */
+
+// includes
 #include <iostream>
 
+// usings
 using std::cout;
 using std::endl;
 
+// main
 int main() {
   int n = 20;
   int arr[n + 1];
+  // we know from 0th step we can reach 1th step by only one way
   arr[1] = 1;
+  // to reach 2nd step, we can directly cum from 0th or 1st step
   arr[2] = 2;
+  // 0 -> 3, 0 -> 1 -> 3, 0 -> 2 -> 3, 0 -> 1 -> 2 -> 3
   arr[3] = 4;
+  /* now we know that each next level will be the sum of number of times we
+    can reach to the last three level */
   for (int i = 4; i <= n; i++) {
     arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
   }
