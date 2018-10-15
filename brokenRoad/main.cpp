37349ebe2611175e3abd45aba4c9911a5aaa5529
Smitkumar Contractor <smitkumarcontractor@Smitkumars-MacBook-Pro.local>
2018-10-15 01:50:02 -0700

    comments

--- brokenRoad/main.cpp
+++ brokenRoad/main.cpp
@@ -1,3 +1,13 @@
+/* file_name : main.cpp
+ * author_name : Smitkumar Contractor
+ * contact : smitcontractor@gmail.com
+ * brief : Given a rod of length n inches and an array of prices that contains
+  prices of all pieces of size smaller than n. Determine the maximum value
+  obtainable by cutting up the rod and selling the pieces. For example, if
+  length of the rod is 8 and the values of different pieces are given as
+  following, then the maximum obtainable value is 22 (by cutting in two pieces
+  of lengths 2 and 6)
+ */
 #include <iostream>
 
 using std::endl;
