5410e05ea1df75fc053eb9c59d7bee5fc35389c2
Smitkumar Contractor <scontractor001@student.butte.edu>
2018-11-07 00:14:44 -0800

    Update main.cpp

--- robHouse/main.cpp
+++ robHouse/main.cpp
@@ -21,38 +21,27 @@ using std::max;
  * returns max money.
  */
 int rob_it(int n, int* h) {
+  // stores maximum value robber can rob (with respect to all house)
   int s[n];
   int temp = 0;
+  // max value robber can rob from house one
   s[0] = h[0];
+  // max value robber can rob from frist two houses
   s[1] = max(h[0], h[1]);
+  // iterate over all remaining houses so in the end we can get real value
   for (int i = 2; i < n; i++) {
-    temp = 0;
-    for (int j = i - 2; j >= 0; j--) {
-      temp = max(temp, s[j]);
-    }
-    s[i] = max(temp + h[i], h[i - 1]);
+    // we can't rob two adj houses
+    // so we have to take maximum of value stolen at prev house and value
+    // and (value stolen at prev prev index + value can be stolen in current
+    // house)
+    s[i] = max(s[i - 2] + h[i], s[i - 1]);
   }
   return s[n - 1];
 }
 
-// int rob_it(int n, int* h) {
-//   int s[n];
-//   int temp = 0;
-//   s[0] = h[0];
-//   s[1] = max(h[0], h[1]);
-//   for (int i = 2; i < n; i++) {
-//     s[i] = s[i - 1];
-//     for (int j = i - 2; j >= 0; j--) {
-//       s[i] = max(s[i], s[j]+h[i]);
-//     }
-//     // s[i] = max(temp + h[i], h[i - 1]);
-//   }
-//   return s[n - 1];
-// }
-
 int main() {
-  int n = 5;
-  int houses[n] = {1, 2, 3, 4 ,5};
+  int n = 7;
+  int houses[n] = {6, 7, 1, 3, 8, 2, 4};
   cout << rob_it(n, houses) << endl;
   return 0;
 }
