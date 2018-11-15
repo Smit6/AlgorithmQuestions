adf8211a92262e6d927a0ace2ac2ca68f14d567c
Smit Contractor <smitcontractor@gmail.com>
2018-11-15 01:24:40 -0800

    commented LIS

--- LIS/main.cpp
+++ LIS/main.cpp
@@ -10,16 +10,24 @@ using std::endl;
 using std::max;
 
 int main() {
+  // size of arr
   int size = 12;
+  // array to store numbers
   int arr[size] = {0, 4, 12, 2, 10, 6, 9, 13, 3, 11, 7, 15};
+  // to store the length of longest increasing subSeq till current index
   int len[12] = {1};
   int subSeq[12] = {0};
   int maximum = 1;
+  // initialize len array
   for (int i = 0; i < size; i++) {
     len[i] = 1;
   }
+  // for each index in our arr we have to find the longest length we can achive
   for (int i = 1; i < size; i++) {
+    // for current index, we will iterate over all the previous indexes
     for (int j = 0; j < i; j++) {
+      // if current index value in array is greater than the value we are
+      // iterating over, than try to increment length at curr array index
       if (arr[i] > arr[j]) {
         len[i] = max(len[i], len[j] + 1);
         subSeq[i] = j;
@@ -27,6 +35,7 @@ int main() {
       }
     }
   }
+  // print possible longest increasing subSeq
   cout << "max = " << maximum << endl;
   return 0;
 }
